a9c107f95e152a0f0edc5d332059d38d0be298e4
gmendozah <geovanimendoza.h@gmail.com>
2021-10-26 10:49:44 -0400

    refactor: update sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -10,37 +10,37 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 songplay_table_create = ("""
 CREATE TABLE
-songplays (songplay_id  INTEGER, start_time BIGINT, user_id INTEGER, level VARCHAR(15), song_id VARCHAR(50), artist_id VARCHAR(50), session_id INTEGER, location VARCHAR(150), user_agent VARCHAR(150));
+songplays (songplay_id SERIAL PRIMARY KEY, start_time BIGINT NOT NULL, user_id INTEGER NOT NULL, level VARCHAR(15), song_id VARCHAR(50), artist_id VARCHAR(50), session_id INTEGER, location VARCHAR(150), user_agent VARCHAR(150));
 """)
 
 user_table_create = ("""
 CREATE TABLE
-users (user_id INTEGER, first_name VARCHAR(150), last_name VARCHAR(150), gender VARCHAR(1), level VARCHAR(15));
+users (user_id VARCHAR(50) PRIMARY KEY, first_name VARCHAR(150), last_name VARCHAR(150), gender VARCHAR(1), level VARCHAR(15));
 """)
 
 song_table_create = ("""
 CREATE TABLE
-songs (song_id VARCHAR(50), title VARCHAR(150), artist_id VARCHAR(50), year INTEGER, duration DECIMAL);
+songs (song_id VARCHAR(50) PRIMARY KEY, title VARCHAR(150), artist_id VARCHAR(50), year INTEGER, duration DECIMAL);
 """)
 
 artist_table_create = ("""
 CREATE TABLE
-artists (artist_id VARCHAR(50), name VARCHAR(150), location VARCHAR(150), latitude TEXT, longitude TEXT);
+artists (artist_id VARCHAR(50) PRIMARY KEY, name VARCHAR(150), location VARCHAR(150), latitude TEXT, longitude TEXT);
 """)
 
 time_table_create = ("""
 CREATE TABLE
-time (start_time TIMESTAMP(3), hour INTEGER, day INTEGER, week INTEGER, month INTEGER, year INTEGER, weekday INTEGER);
+time (start_time TIMESTAMP(3) PRIMARY KEY, hour INTEGER, day INTEGER, week INTEGER, month INTEGER, year INTEGER, weekday INTEGER);
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s);
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET first_name = excluded.first_name, last_name = excluded.last_name, gender = excluded.gender, level = excluded.level;
 """)
 
 song_table_insert = ("""
@@ -48,18 +48,18 @@ INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);
+INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
-INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
+INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
-SELECT s.song_id, a.artist_id FROM songs s JOIN artists a ON s.song_id = a.artist_id WHERE s.title = %s AND a.name = %s AND s.duration = %s;
+SELECT s.song_id, s.artist_id FROM songs s JOIN artists a ON a.artist_id = s.artist_id WHERE (s.title = %s AND a.name = %s AND s.duration = %s);
 """)
 
 # QUERY LISTS
